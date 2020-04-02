7c576e1b1f245d8e7da91bfeb16602dd4406022c
Anastasiya Fedorova <av.fedorova@outlook.com>
2020-04-02 22:33:02 +0300

    Добавлена фильтрация в списке заявок

--- resources/views/allRequests.blade.php
+++ resources/views/allRequests.blade.php
@@ -2,8 +2,16 @@
 
 @section('content')
 <h1>Все заявки</h1>
+<div id="myBtnContainer">
+  <button class="btn active" onclick="filterSelection('all')"> Показать все</button>
+  <button class="btn" onclick="filterSelection('status-0')"> Непросмотреные</button>
+  <button class="btn" onclick="filterSelection('status-1')"> Просмотреные</button>
+  <button class="btn" onclick="filterSelection('status-2')"> Принятые</button>
+  <button class="btn" onclick="filterSelection('status-3')"> Закрытые</button>
+</div>
+
     @foreach($data as $el)
-    <div class="alert alert-info">
+    <div class="alert alert-info filterDiv status-{{$el->status}}">
         <h3>{{ $el->title }}</h3>
         <p>{{ $el->message_user}}</p>
         <p><small>{{$el->getstatus()}}</small></br>
