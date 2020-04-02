0d5f59cb1361b2ee8a564740468f731fdab16a21
Anastasiya Fedorova <av.fedorova@outlook.com>
2020-04-02 09:13:05 +0300

    Добавлен признак Пользователь является менеджером

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,26 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+class DatabaseSeeder extends Seeder {
+
+	public function run()
+	{
+		$this->call('UserTableSeeder');
+
+		$this->command->info('Таблица пользователей заполнена данными!');
+	}
+
+}
+
+class UserTableSeeder extends Seeder {
+
+	public function run()
+	{
+        DB::table('users')->delete();
+        $password = Hash::make('manager');
+		User::create(array('name' => 'manager', 'email' => 'av.fedorova@mail.ru', 'is_manager' => true, 'password' => $password));
+	}
+
 }
